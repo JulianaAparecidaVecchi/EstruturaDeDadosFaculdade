26151fa95cefa2e4a53dbcff79486e1689c6254e
Juliana <julianaaparecidavecchi@gmail.com>
2025-09-03 08:59:33 -0300

    Resolução dos exercícios da Aula04

---
+++ Aula04ED-Array/src/Ex6/Main.java
@@ -0,0 +1,24 @@
+package Ex6;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(3);
+        numeros.add(4);
+        numeros.add(5);
+        numeros.add(6);
+        numeros.add(7);
+        numeros.add(8);
+        numeros.add(9);
+        numeros.add(10);
+        numeros.add(11);
+        numeros.add(12);
+
+        System.out.println(numeros);
+    }
+}
